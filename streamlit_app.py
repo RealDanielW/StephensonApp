984ed9329a70f9e90e469773b0a8e8d144021b18
dave-dhcontrols <technical@dhcontrols.co.uk>
2025-04-11 14:29:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,7 +152,7 @@ metrics = [
 cols = st.columns(4)
 for col, (title, column, color) in zip(cols, metrics):
     total_value = df[column].sum()
-    display_metric(col, title, total_value, df_display, column, color, time_frame)
+    #display_metric(col, title, total_value, df_display, column, color, time_frame)
 
 st.subheader("Power Usage (KWH) Selected Duration")
 
